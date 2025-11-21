d171fe71c193ffd9ea7cc4a2bcf47d0bf4576ddf
Chennupalli Triveni <chennupallitriveni2003@gmail.com>
2025-11-21 21:11:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+// All sound names
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+
+// Create audio objects
+const audioMap = {};
+
+sounds.forEach(sound => {
+    audioMap[sound] = new Audio(`./sounds/${sound}.mp3`);
+});
+
+// Play selected sound
+document.querySelectorAll(".btn").forEach(btn => {
+    btn.addEventListener("click", () => {
+        stopAllSounds();
+        const soundName = btn.dataset.sound;
+        audioMap[soundName].play();
+    });
+});
+
+// Stop button
+document.querySelector(".stop").addEventListener("click", stopAllSounds);
+
+// Function to stop all sounds
+function stopAllSounds() {
+    sounds.forEach(sound => {
+        audioMap[sound].pause();
+        audioMap[sound].currentTime = 0;
+    });
+}
